c8747ad0f6cd31420b2571728b54bf56355ee133
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2024-12-10 08:04:02 +0700

    thuyr

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/animations.css">  
     <link rel="stylesheet" href="css/main.css">  
-    <link rel="stylesheet" href="css/index.css">
+    <lignk rel="stylesheet" href="css/index.css">
     <title>eDoc</title>
     <style>
         table{
